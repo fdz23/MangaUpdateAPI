4b3ae8da9c01b0a9a838254b419f74bc85b4cb94
Fernando David Teixeira Meira <ferdavid_9@hotmail.com>
2022-03-06 12:17:51 -0300

    Created Http Helper

---
+++ MangaUpdateAPI/Util/HttpHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace MangaUpdateAPI.Util
+{
+    public static class HttpHelper
+    {
+        public static HttpRequestMessage GetValidRequest()
+        {
+            var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Head;
+            request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
+
+            return request;
+        }
+    }
+}
