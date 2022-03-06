a6ca059bae3bd275bb5980e7c255ed7801c6bcf8
Fernando David Teixeira Meira <ferdavid_9@hotmail.com>
2022-03-06 12:17:58 -0300

    Created Url Helper

---
+++ MangaUpdateAPI/Util/UrlHelper.cs
@@ -0,0 +1,31 @@
+﻿using MangaUpdateAPI.Model;
+
+namespace MangaUpdateAPI.Util
+{
+    public static class UrlHelper
+    {
+        public static string GetNextChapterUrl(Manga manga)
+        {
+            var nextChapter = manga.ObtainNextChapter();
+
+            var split = manga.Url.Split(manga.LastChapterRead);
+            if (split.Length == 1)
+                return null;
+
+            if (split.Length == 2)
+                return String.Join(nextChapter.ToString(), split);
+
+            if (split.Length > 2)
+            {
+                var lastSplit = split[split.Length - 1];
+
+                var newSplit = split.SkipLast(1);
+                var url = String.Join(manga.LastChapterRead, newSplit);
+
+                return url + nextChapter + lastSplit;
+            }
+
+            return null;
+        }
+    }
+}
